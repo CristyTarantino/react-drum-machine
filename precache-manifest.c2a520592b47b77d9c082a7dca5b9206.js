2f309d5bf1ec6971fa3c13e276394dcec71c0a53
Cristina Tarantino <cristina@versantus.co.uk>
2020-08-04 13:48:08 +0100

    Updates

---
+++ precache-manifest.c2a520592b47b77d9c082a7dca5b9206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c09ab6c6e617d17ddd393c81b52a20",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "b54bbf6e2177067f43e1",
+    "url": "/react-drum-machine/static/css/main.abecb2b5.chunk.css"
+  },
+  {
+    "revision": "8d7bbcd30517dc4a0f17",
+    "url": "/react-drum-machine/static/js/2.416a2fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-drum-machine/static/js/2.416a2fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54bbf6e2177067f43e1",
+    "url": "/react-drum-machine/static/js/main.f6d9b1ab.chunk.js"
+  },
+  {
+    "revision": "4326b2b5101aeb4c5d5f",
+    "url": "/react-drum-machine/static/js/runtime-main.c69e298b.js"
+  }
+]);
